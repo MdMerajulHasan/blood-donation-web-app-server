23171f76c9acb5980095e6ec3b3bb0d83d8fce6e
Jim <merajuljim@gmai.com>
2025-12-13 13:39:54 +0600

    simple server created only posting user data api added

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.Mongodb_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Blood Donation Server is Running!");
+});
+
+// the run function of mongodb. inside it all the data management is done
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    // create the database in mongodb
+    const db = client.db("bloodDonation");
+    // creating the data collection for the users data under the database "bloodDonation"
+    const bloodDonationUsersCollection = db.collection("usersCollection");
+
+    // all the api
+    // api to store users data in usersCollection collection
+    app.post("/users", async (req, res) => {
+      const result = await bloodDonationUsersCollection.insertOne(req.body);
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Blood Donation App is Running At Port: `, port);
+});
